6fd17bdf4cb9b4c68e27a1e16a73b175cfe3ff26
paul <javafxdev@gmail.com>
2018-07-25 20:35:58 +0200

    ws security init

--- src/main/java/org/springframework/samples/petclinic/security/MyWSInterceptor.java
+++ src/main/java/org/springframework/samples/petclinic/security/MyWSInterceptor.java
@@ -1,4 +1,34 @@
 package org.springframework.samples.petclinic.security;
 
-public class MyWSInterceptor {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ws.context.MessageContext;
+
+public class MyWSInterceptor implements org.springframework.ws.server.EndpointInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(MyWSInterceptor.class);
+
+    @Override
+    public boolean handleRequest(MessageContext messageContext, Object o) throws Exception {
+        log.info("handleRequest");
+        return true;
+    }
+
+    @Override
+    public boolean handleResponse(MessageContext messageContext, Object o) throws Exception {
+        log.info("handleResponse");
+        return true;
+    }
+
+    @Override
+    public boolean handleFault(MessageContext messageContext, Object o) throws Exception {
+        log.info("handleFault");
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(MessageContext messageContext, Object o, Exception e) throws Exception {
+        log.info("afterCompletion");
+
+    }
 }
--- src/main/java/org/springframework/samples/petclinic/security/WsSecurity.java
+++ src/main/java/org/springframework/samples/petclinic/security/WsSecurity.java
@@ -1,4 +1,51 @@
 package org.springframework.samples.petclinic.security;
 
-public class WsSecurity {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.server.endpoint.interceptor.PayloadLoggingInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
+import org.springframework.ws.soap.security.xwss.callback.SpringDigestPasswordValidationCallbackHandler;
+import org.springframework.ws.soap.security.xwss.callback.SpringPlainTextPasswordValidationCallbackHandler;
+
+import javax.security.auth.callback.CallbackHandler;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+@Configuration
+@EnableWs
+public class WsSecurity extends WsConfigurerAdapter {
+
+    @Value("classpath:securityPolicy.xml")
+    private Resource securityPolicy;
+
+    @Override
+    public void addInterceptors(List<EndpointInterceptor> interceptors) {
+        interceptors.add(new MyWSInterceptor());
+        PayloadLoggingInterceptor loggingInterceptor = new PayloadLoggingInterceptor();
+        loggingInterceptor.setLogRequest(true);
+        interceptors.add(loggingInterceptor);
+        //interceptors.add(wsSecurityInterceptor());
+    }
+
+    //@Bean("wsSecurityInterceptor")
+    XwsSecurityInterceptor wsSecurityInterceptor() {
+        XwsSecurityInterceptor interceptor = new XwsSecurityInterceptor();
+        interceptor.setPolicyConfiguration(securityPolicy);
+        interceptor.setCallbackHandlers(new CallbackHandler[]{handler()});
+        return interceptor;
+    }
+
+    SpringPlainTextPasswordValidationCallbackHandler handler(){
+        return new SpringPlainTextPasswordValidationCallbackHandler();
+    }
+
+
 }
